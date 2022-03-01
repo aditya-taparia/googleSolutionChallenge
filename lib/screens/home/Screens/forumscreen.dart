79573448353561e25097d66e324147c6df47b000
Som Sagar <somsagar2010@gmail.com>
2022-03-02 03:28:06 +0530

    map_edit

--- lib/screens/home/Screens/forumscreen.dart
+++ lib/screens/home/Screens/forumscreen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:googlesolutionchallenge/screens/home/Screens/addforum.dart';
 import 'package:googlesolutionchallenge/screens/home/Screens/forummodel.dart';
 
 class ForumScreen extends StatefulWidget {
@@ -21,123 +24,128 @@ class _ForumScreenState extends State<ForumScreen> {
               itemCount: forum.length,
               itemBuilder: (BuildContext context, int index) {
                 final forums = forum[index];
-                return Expanded(
+                return Container(
+                  margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 1),
                   child: Container(
-                    margin: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
-                    child: Container(
-                      color: Colors.white,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  child: Row(
-                                    children: [
-                                      CircleAvatar(),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            forums.sender.name,
-                                            style: TextStyle(fontSize: 18),
-                                          ),
-                                          Text(
-                                            'Location',
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Text(
-                                  forums.time,
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 7,
-                            ),
-                            Container(
-                              child: Text(
-                                forums.msg,
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.black),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: SizedBox(
-                                height: 20,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
                                 child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(
-                                      forums.numLiked.toString() + ' Likes',
-                                      style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.bold),
+                                    CircleAvatar(
+                                      backgroundColor: Colors.primaries[Random()
+                                          .nextInt(Colors.primaries.length)],
+                                      child: Text(
+                                        forums.sender.name[0],
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                        ),
+                                      ),
                                     ),
-                                    Text('5 Comments',
-                                        style: TextStyle(
-                                            color: Colors.grey,
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.bold)),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          forums.sender.name,
+                                          style: TextStyle(fontSize: 18),
+                                        ),
+                                        const Text(
+                                          'Location',
+                                          style: TextStyle(
+                                              fontSize: 12, color: Colors.grey),
+                                        ),
+                                      ],
+                                    ),
                                   ],
                                 ),
                               ),
+                              Text(
+                                forums.time,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            forums.msg,
+                            style: const TextStyle(
+                                fontSize: 15, color: Colors.black),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: SizedBox(
+                              height: 20,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    forums.numLiked.toString() + ' Likes',
+                                    style: const TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const Text('5 Comments',
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
                             ),
-                            Divider(
-                              thickness: 0.2,
-                              color: Colors.black,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {},
-                                  child: Icon(
-                                    Icons.favorite,
-                                    color: forums.isLiked
-                                        ? Colors.pink
-                                        : Colors.white,
-                                  ),
+                          ),
+                          const Divider(
+                            thickness: 0.2,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              GestureDetector(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: forums.isLiked
+                                      ? Colors.pink
+                                      : Colors.white,
                                 ),
-                                GestureDetector(
-                                  onTap: () {},
-                                  child: Icon(
-                                    Icons.comment,
-                                    color: Color.fromRGBO(66, 103, 178, 1),
-                                  ),
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: const Icon(
+                                  Icons.comment,
+                                  color: Color.fromRGBO(66, 103, 178, 1),
                                 ),
-                                GestureDetector(
-                                  onTap: () {},
-                                  child: Icon(
-                                    Icons.report_rounded,
-                                    // color: Colors.white,
-                                  ),
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: const Icon(
+                                  Icons.report_rounded,
+                                  // color: Colors.white,
                                 ),
-                              ],
-                            )
-                          ],
-                        ),
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ),
@@ -149,8 +157,9 @@ class _ForumScreenState extends State<ForumScreen> {
           child: Align(
             alignment: Alignment.bottomRight,
             child: FloatingActionButton(
-              onPressed: () {},
-              child: Icon(
+              onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Addforum())),
+              child: const Icon(
                 Icons.add,
               ),
             ),
