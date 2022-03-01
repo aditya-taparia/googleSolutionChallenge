425f88ad127ea226b4822ab5dff7496aea93229c
Som Sagar <somsagar2010@gmail.com>
2022-03-01 19:59:27 +0530

    geolocation

--- lib/screens/home/Screens/map.dart
+++ lib/screens/home/Screens/map.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -9,14 +12,56 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController myMapController;
-  static const LatLng _mainLocation = const LatLng(25.69893, 32.6421);
+  Completer<GoogleMapController> _controller = Completer();
+  LatLng currentPostion = LatLng(25.69893, 32.6421);
+  late LocationPermission permission;
+
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
+    return Stack(children: [
+      GoogleMap(
+        zoomControlsEnabled: false,
         initialCameraPosition: CameraPosition(
-      target: _mainLocation,
-      zoom: 10.0,
-    ));
+          target: currentPostion,
+          zoom: 10.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+              backgroundColor: const Color.fromRGBO(66, 103, 178, 1),
+              onPressed: () => getLocation(),
+              child: Icon(Icons.location_pin)),
+        ),
+      )
+    ]);
+  }
+
+  getLocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final GoogleMapController controller = await _controller.future;
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    controller.animateCamera(
+        CameraUpdate.newLatLng(LatLng(position.latitude, position.longitude)));
+    print(currentPostion);
   }
 }
