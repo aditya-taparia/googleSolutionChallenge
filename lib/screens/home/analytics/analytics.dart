060632790f34dd8c868b73d2551b3a574fb14a48
Som Sagar <somsagar2010@gmail.com>
2022-03-02 20:18:43 +0530

    analytics_struct/mapload

--- lib/screens/home/analytics/analytics.dart
+++ lib/screens/home/analytics/analytics.dart
@@ -10,8 +10,79 @@ class Analytics extends StatefulWidget {
 class _AnalyticsState extends State<Analytics> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Analytics"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          color: const Color.fromRGBO(66, 103, 178, 1),
+          width: MediaQuery.of(context).size.width,
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Earnings   ",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Donations  ",
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ],
+            ),
+          ),
+        ),
+        DefaultTabController(
+          length: 2,
+          initialIndex: 0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: const Color.fromRGBO(66, 103, 178, 1),
+                child: const TabBar(
+                    indicatorColor: Colors.white,
+                    unselectedLabelColor: Colors.white70,
+                    tabs: [
+                      Tab(
+                        child: Text(
+                          'Charts',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      Tab(
+                          child: Text(
+                        'History',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                    ]),
+              ),
+              Container(
+                height: 300,
+                child: TabBarView(children: [
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 1',
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 2',
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ]),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
