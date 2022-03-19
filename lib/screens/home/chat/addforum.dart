85cad8b1032ac4f4a2734820ca397cf6c94e65da
Som Sagar <somsagar2010@gmail.com>
2022-03-20 05:11:12 +0530

    linkspacefeedDB

--- lib/screens/home/chat/addforum.dart
+++ lib/screens/home/chat/addforum.dart
@@ -1,81 +1,131 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:googlesolutionchallenge/models/user.dart';
+import 'package:googlesolutionchallenge/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 class Addforum extends StatefulWidget {
-  const Addforum({Key? key}) : super(key: key);
+  final String id;
+  const Addforum({Key? key, required this.id}) : super(key: key);
 
   @override
   _AddforumState createState() => _AddforumState();
 }
 
 class _AddforumState extends State<Addforum> {
+  final _feedcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Share Post"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 28,
-                  backgroundColor: Colors
-                      .primaries[Random().nextInt(Colors.primaries.length)],
-                  child: const Text(
-                    "S",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Column(
+    final user = Provider.of<Users?>(context);
+    final Stream<DocumentSnapshot> _userStream = FirebaseFirestore.instance
+        .collection('Userdata')
+        .doc(user!.userid)
+        .snapshots();
+
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _userStream,
+        builder: (BuildContext context,
+            AsyncSnapshot<DocumentSnapshot<Object?>> userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          }
+          if (userSnapshot.hasData) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Share Post"),
+              ),
+              body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Som Sagar",
-                      style: TextStyle(fontSize: 18),
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 28,
+                          backgroundColor: Colors.primaries[
+                              Random().nextInt(Colors.primaries.length)],
+                          child: Text(
+                            userSnapshot.data!['name'][0],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              userSnapshot.data!['name'],
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            const Text(
+                              'Location',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Location',
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    SizedBox(
+                      height: 300,
+                      //  color: Colors.black,
+                      child: TextField(
+                        controller: _feedcontroller,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          labelText: 'Post',
+                          hintText: 'What do you want to talk about ?',
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "Add Hashtag",
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 300,
-              //  color: Colors.black,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  labelText: 'Post',
-                  hintText: 'What do you want to talk about ?',
-                ),
               ),
-            ),
-            const Spacer(),
-            const Text(
-              "Add Hashtag",
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  sendpost(
+                      _feedcontroller, user.userid, userSnapshot.data!['name']);
+                  _feedcontroller.clear();
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.done),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            );
+          }
+          return const Loading();
+        });
+  }
+
+  Future sendpost(
+      TextEditingController feedcontroller, String userid, param2) async {
+    final feed = FirebaseFirestore.instance.collection("Feeds").doc();
+    final json = {
+      'Likes': 0,
+      'Name': param2,
+      'date': "1/1/1",
+      'feedtype': widget.id,
+      'ownerid': userid,
+      'postbody': feedcontroller.text,
+      'title': "default"
+    };
+
+    await feed.set(json);
   }
 }
--- lib/screens/home/chat/forumscreen.dart
+++ lib/screens/home/chat/forumscreen.dart
@@ -1,184 +1,217 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:googlesolutionchallenge/screens/home/chat/addforum.dart';
 import 'package:googlesolutionchallenge/screens/home/chat/forummodel.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:googlesolutionchallenge/widgets/loading.dart';
 
 class ForumScreen extends StatefulWidget {
-  const ForumScreen({Key? key}) : super(key: key);
+  final String id;
+  const ForumScreen({Key? key, required this.id}) : super(key: key);
 
   @override
   _ForumScreenState createState() => _ForumScreenState();
 }
 
 class _ForumScreenState extends State<ForumScreen> {
+  final feeds = FirebaseFirestore.instance.collection('Feeds');
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-          ),
-          child: ListView.builder(
-            itemCount: forum.length,
-            itemBuilder: (BuildContext context, int index) {
-              final forums = forum[index];
-              return Container(
-                margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 1),
-                child: Container(
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor: Colors.primaries[Random()
-                                      .nextInt(Colors.primaries.length)],
-                                  child: Text(
-                                    forums.sender.name[0],
+    return StreamBuilder<QuerySnapshot>(
+        stream: feeds.where('feedtype', isEqualTo: widget.id).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+          if (!snapshot.hasData) {
+            const Loading();
+          }
+          if (snapshot.connectionState == ConnectionState.active) {
+            var feedinfo = snapshot.data!.docs;
+
+            if (feedinfo.isEmpty) {
+              return Text('no Feed');
+            } else {
+              return Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                    ),
+                    child: ListView.builder(
+                      itemCount: feedinfo.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 1),
+                          child: Container(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          CircleAvatar(
+                                            backgroundColor: Colors.primaries[
+                                                Random().nextInt(
+                                                    Colors.primaries.length)],
+                                            child: Text(
+                                              feedinfo[index]['Name'][0],
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 18,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                feedinfo[index]['Name'],
+                                                style: const TextStyle(
+                                                    fontSize: 18),
+                                              ),
+                                              const Text(
+                                                'Location',
+                                                style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.grey),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        feedinfo[index]['date'],
+                                        style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 7,
+                                  ),
+                                  Text(
+                                    feedinfo[index]['postbody'],
                                     style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
+                                        fontSize: 15, color: Colors.black),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 8.0),
+                                    child: SizedBox(
+                                      height: 20,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            feedinfo[index]['Likes']
+                                                    .toString() +
+                                                ' Likes',
+                                            style: const TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const Text('5 Comments',
+                                              style: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.bold)),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      forums.sender.name,
-                                      style: const TextStyle(fontSize: 18),
-                                    ),
-                                    const Text(
-                                      'Location',
-                                      style: TextStyle(
-                                          fontSize: 12, color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Text(
-                              forums.time,
-                              style: const TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
+                                  const Divider(
+                                    thickness: 0.2,
+                                    color: Colors.black,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: Icon(Icons.favorite,
+                                            color: Colors.pink),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.comment,
+                                          color:
+                                              Color.fromRGBO(66, 103, 178, 1),
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.report_rounded,
+                                          // color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 7,
-                        ),
-                        Text(
-                          forums.msg,
-                          style: const TextStyle(
-                              fontSize: 15, color: Colors.black),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: SizedBox(
-                            height: 20,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  forums.numLiked.toString() + ' Likes',
-                                  style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const Text('5 Comments',
-                                    style: TextStyle(
-                                        color: Colors.grey,
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const Divider(
-                          thickness: 0.2,
-                          color: Colors.black,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            GestureDetector(
-                              onTap: () {},
-                              child: Icon(
-                                Icons.favorite,
-                                color:
-                                    forums.isLiked ? Colors.pink : Colors.white,
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {},
-                              child: const Icon(
-                                Icons.comment,
-                                color: Color.fromRGBO(66, 103, 178, 1),
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {},
-                              child: const Icon(
-                                Icons.report_rounded,
-                                // color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-              alignment: Alignment.bottomRight,
-              child: SpeedDial(
-                animatedIcon: AnimatedIcons.menu_home,
-                children: [
-                  SpeedDialChild(
-                      backgroundColor: const Color.fromRGBO(66, 103, 178, 1),
-                      label: "Post",
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Addforum(),
                           ),
                         );
-                      }),
-                  SpeedDialChild(
-                      backgroundColor: const Color.fromRGBO(66, 103, 178, 1),
-                      label: "Open",
-                      child: const Icon(Icons.add, color: Colors.white)),
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: SpeedDial(
+                          animatedIcon: AnimatedIcons.menu_home,
+                          children: [
+                            SpeedDialChild(
+                                backgroundColor:
+                                    const Color.fromRGBO(66, 103, 178, 1),
+                                label: "Post",
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          Addforum(id: widget.id),
+                                    ),
+                                  );
+                                }),
+                            SpeedDialChild(
+                                backgroundColor:
+                                    const Color.fromRGBO(66, 103, 178, 1),
+                                label: "Open",
+                                child:
+                                    const Icon(Icons.add, color: Colors.white)),
+                          ],
+                        )),
+                  )
                 ],
-              )),
-        )
-      ],
-    );
+              );
+            }
+          }
+          return Loading();
+        });
   }
 }
