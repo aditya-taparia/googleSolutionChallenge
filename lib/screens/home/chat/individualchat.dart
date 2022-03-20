6c4e66b085aa2e1f8f35ba9ab027bc8a04054467
Som Sagar <somsagar2010@gmail.com>
2022-03-21 02:52:19 +0530

    chat

--- lib/screens/home/chat/individualchat.dart
+++ lib/screens/home/chat/individualchat.dart
@@ -1,16 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:googlesolutionchallenge/models/user.dart';
 import 'package:googlesolutionchallenge/screens/home/chat/sample.dart';
+import 'package:googlesolutionchallenge/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 class IndividualChat extends StatefulWidget {
-  const IndividualChat({Key? key, required this.user}) : super(key: key);
-  final User user;
+  const IndividualChat({Key? key, required this.user, required this.id})
+      : super(key: key);
+  final Map user;
+  final String id;
 
   @override
   _IndividualChatState createState() => _IndividualChatState();
 }
 
 class _IndividualChatState extends State<IndividualChat> {
-  buildMessage(Message message, bool isUser) {
+  final _msgcontroller = TextEditingController();
+  buildMessage(String message, String time, bool isUser) {
     return Container(
       margin: isUser
           ? const EdgeInsets.only(top: 10, bottom: 8, left: 80)
@@ -26,13 +33,13 @@ class _IndividualChatState extends State<IndividualChat> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            message.text,
+            message,
             style: TextStyle(color: isUser ? Colors.white : Colors.black),
           ),
           Align(
             alignment: Alignment.bottomRight,
             child: Text(
-              message.time,
+              time,
               style: TextStyle(
                   color: !isUser ? Colors.grey : Colors.white, fontSize: 12),
             ),
@@ -42,7 +49,7 @@ class _IndividualChatState extends State<IndividualChat> {
     );
   }
 
-  buildMessageComposer() {
+  buildMessageComposer(String userid, length) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8),
       height: 70,
@@ -56,6 +63,7 @@ class _IndividualChatState extends State<IndividualChat> {
         ),
         Expanded(
             child: TextField(
+          controller: _msgcontroller,
           textCapitalization: TextCapitalization.sentences,
           onChanged: (value) {},
           decoration: const InputDecoration.collapsed(
@@ -63,7 +71,13 @@ class _IndividualChatState extends State<IndividualChat> {
           ),
         )),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            print(_msgcontroller.text);
+
+            String msg = _msgcontroller.text;
+            addmsgDB(msg, widget.id, userid, length);
+            _msgcontroller.clear();
+          },
           icon: const Icon(Icons.send),
           iconSize: 30,
           color: Theme.of(context).primaryColor,
@@ -74,43 +88,77 @@ class _IndividualChatState extends State<IndividualChat> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(66, 103, 178, 1),
-      appBar: AppBar(
-        title: Text(widget.user.name),
-        elevation: 0,
-      ),
-      body: GestureDetector(
-        onTap: (() => FocusScope.of(context).unfocus()),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30)),
-                  child: ListView.builder(
-                      reverse: true,
-                      padding: const EdgeInsets.only(top: 15),
-                      itemCount: messages.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final Message message = messages[index];
-                        final bool isUser = message.sender.id == currentUser.id;
-                        return buildMessage(message, isUser);
-                      }),
+    final user = Provider.of<Users?>(context);
+    return StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats')
+            .doc(widget.id)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          }
+          if (snapshot.connectionState == ConnectionState.active) {
+            print(snapshot.data!['chatdata']);
+            Map data = snapshot.data!['chatdata'];
+            return Scaffold(
+              backgroundColor: const Color.fromRGBO(66, 103, 178, 1),
+              appBar: AppBar(
+                title: Text(widget.user["name"]),
+                elevation: 0,
+              ),
+              body: GestureDetector(
+                onTap: (() => FocusScope.of(context).unfocus()),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30))),
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30)),
+                          child: ListView.builder(
+                              padding: const EdgeInsets.only(top: 15),
+                              itemCount: data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                print(data[index.toString()]);
+
+                                final bool isUser =
+                                    data[index.toString()][0] == user!.userid;
+                                return buildMessage(
+                                    data[index.toString()][1],
+                                    data[index.toString()][2].toString(),
+                                    isUser);
+                              }),
+                        ),
+                      ),
+                    ),
+                    buildMessageComposer(
+                        user!.userid, snapshot.data!['chatdata'].length),
+                  ],
                 ),
               ),
-            ),
-            buildMessageComposer(),
-          ],
-        ),
-      ),
-    );
+            );
+          } else {
+            return Text("kuch nhi hua");
+          }
+        });
+  }
+
+  Future addmsgDB(String msg, String id, userid, length) async {
+    final linkspace = FirebaseFirestore.instance.collection("chats").doc(id);
+    var time = DateTime.now();
+
+    linkspace.set({
+      "chatdata": {
+        length.toString(): [userid, msg, time]
+      }
+    }, SetOptions(merge: true));
   }
 }
