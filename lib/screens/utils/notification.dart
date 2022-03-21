3050a103e71747a39f49924511b44be9225810b8
Som Sagar <somsagar2010@gmail.com>
2022-03-22 04:31:48 +0530

    notificationsDB

--- lib/screens/utils/notification.dart
+++ lib/screens/utils/notification.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:googlesolutionchallenge/models/user.dart';
+import 'package:googlesolutionchallenge/widgets/loading.dart';
+import 'package:googlesolutionchallenge/widgets/loading_cards.dart';
+import 'package:provider/provider.dart';
 
 class Notify extends StatefulWidget {
   const Notify({Key? key}) : super(key: key);
@@ -7,52 +12,85 @@ class Notify extends StatefulWidget {
   State<Notify> createState() => _NotifyState();
 }
 
-List<String> notifications = [
-  "2000/- transferred from Kowsik",
-  "your post was liked",
-  "your service request has been approved"
-];
-List<int> notificationsDay = [1, 5, 4];
-
-List<int> notificationsType = [1, 0, 2];
-
 class _NotifyState extends State<Notify> {
+  bool showcontainer = false;
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<Users?>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Inbox"),
       ),
-      body: ListView.builder(
-          itemCount: notifications.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: notificationsType[index] == 0
-                  ? const Icon(
-                      Icons.volunteer_activism,
-                      color: Colors.pink,
-                    )
-                  : notificationsType[index] == 1
-                      ? const Icon(
-                          Icons.account_balance,
-                          color: Colors.blueAccent,
-                        )
-                      : const Icon(
-                          Icons.check_circle,
-                          color: Colors.green,
-                        ),
-              title: Text(notifications[index]),
-              subtitle: Text(
-                notificationsDay[index].toString() + "D",
-                style: const TextStyle(color: Colors.grey),
-              ),
-              trailing: IconButton(
-                icon: const Icon(
-                  Icons.more_vert,
-                ),
-                onPressed: () {},
-              ),
-            );
+      body: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('Userdata')
+              .doc(user!.userid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const LoadingCard();
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const LoadingCard();
+            }
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!["notifications"].length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: const Icon(Icons.notifications_active),
+                      title: Text(
+                          snapshot.data!["notifications"][index.toString()][0]),
+                      subtitle: Text(
+                        snapshot.data!["notifications"][index.toString()][1],
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                      trailing: Stack(
+                        children: [
+                          IconButton(
+                            icon: const Icon(
+                              Icons.more_vert,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                showcontainer = true;
+                              });
+                            },
+                          ),
+                          showcontainer
+                              ? Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    color: Colors.white,
+                                    width: 80,
+                                    height: 40,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("Delete"),
+                                        GestureDetector(
+                                          child: Icon(
+                                            Icons.delete_outline,
+                                            color: Colors.red,
+                                          ),
+                                          onTap: () {
+                                            setState(() {
+                                              showcontainer = false;
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              : Text("")
+                        ],
+                      ),
+                    );
+                  });
+            }
+            return Loading();
           }),
     );
   }
